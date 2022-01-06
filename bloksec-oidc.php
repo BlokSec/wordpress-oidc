c95ec84ee94cef81ad4f8ac0d3020ca0916f5b8f
Kevin Wicken <kwicken@bloksec.com>
2022-01-05 20:23:41 -0500

    version bump

--- bloksec-oidc.php
+++ bloksec-oidc.php
@@ -16,7 +16,7 @@
  * Plugin Name:       BlokSec OpenID Connect
  * Plugin URI:        https://bloksec.com
  * Description:       Connect to an BlokSec OpenID Connect client.
- * Version:           3.3.0
+ * Version:           3.3.1
  * Author:            BlokSec Inc.
  * Author URI:        https://github.com/wicken
  * Text Domain:       bloksec-oidc
@@ -79,7 +79,7 @@ class BlokSec_OIDC {
 	 *
 	 * @var
 	 */
-	const VERSION = '3.3.0';
+	const VERSION = '3.3.1';
 
 	/**
 	 * Plugin settings.
